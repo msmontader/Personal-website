e97f4b8db232ac0cbeefdcb18611565cc17dab0a
Muntadher Inaya <msmontader@gmail.com>
2017-01-10 22:26:44 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ document.getElementsByTagName("h1")[0].style.fontSize = "80px";
     vph = $(window).height();
 
     $('.full-page').height(vph);
-$('#body').css('min-height', '100%');
+$('.pageOne').css('min-height', '100%');
+$('.pageTwo').css('min-height', '100%');
+$('.pageThree').css('min-height', '100%');
