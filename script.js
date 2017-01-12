95c81a80cc6ab82bb62947633f46e0631fd1b963
Muntadher Inaya <msmontader@gmail.com>
2017-01-11 18:04:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,19 +10,4 @@ $('.pageThree').css('min-height', '100%');
 $(window).scroll(function(){
   $("#nav_div").css({"top": ($(window).scrollTop()) + "px"});
 });
-$(document).ready(function(){
-  $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
-    }
-  });
-});
+$('.nav').localScroll();
