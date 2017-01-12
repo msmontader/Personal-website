a976ac9ed9426cce1e76e72a91762cb2c77d3783
Muntadher Inaya <msmontader@gmail.com>
2017-01-12 13:56:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ $(document).ready(function() {
 document.getElementsByTagName("h1")[0].style.fontSize = "80px";
     vpw = $(window).width();
     vph = $(window).height();
-$(window).scroll(function(){
-  $("#nav_div").css({"top": ($(window).scrollTop()) + "px"});
 });
 $('.nav').localScroll();
